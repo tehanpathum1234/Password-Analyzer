0eea337f76d90c3d05c4c601ea0530284cc8fd27
tehanpathum1234 <tehanpathum1234@gmail.com>
2026-05-29 03:10:42 +0100

    app.py

--- app.py
+++ app.py
@@ -109,4 +109,4 @@ def index():
   return render_template("index.html", result=result, user_name=user_name)
 
 if __name__ == '__main__' :
-  app.run(debug=True)
+  app.run(host="0.0.0.0", port=5000)
